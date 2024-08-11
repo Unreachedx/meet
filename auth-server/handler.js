ba50431e04dd822bfeaace738792f2a1cbef0329
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-08-11 18:32:55 +0200

    Updates

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,7 +4,9 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = [ "https://unreachedx.github.io/meet/" ];
+const redirect_uris = [
+  "https://unreachedx.github.io/meet/"
+];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -13,11 +15,6 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array is passed to the `scope` option. 
-   *
-   */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -25,12 +22,85 @@ module.exports.getAuthURL = async () => {
 
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
+    headers: { "Access-Control-Allow-Origin": "*" },
     body: JSON.stringify({
       authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+
+      const access_token = response.access_token;
+
+      oAuth2Client.setCredentials({ access_token });
+
+      // Get Callender events
+      calendar.events.list(
+        {
+          calendarId: CALENDAR_ID,
+          auth: oAuth2Client,
+          timeMin: new Date().toISOString(), // Retrieves events starting from now
+          maxResults: 10, // Limit the number of results
+          singleEvents: true,
+          orderBy: 'startTime',
+        },
+        (err, res) => {
+          if (err) return reject(err);
+          return resolve(res.data.items);
+        }
+      );
+    });
+  })
+  .then((results) => {
+    return {
+      statusCode: 200,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify(results),
+    };
+  })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 };
