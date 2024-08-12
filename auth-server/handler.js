f8809586f796d9c583c28328410a5158d9245902
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-08-12 18:54:58 +0200

    updated

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,7 +6,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/calendar.events.public.readonly",
 ];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://alionaterguta.github.io/meet/"];
+const redirect_uris = ["https://unreachedx.github.io/meet/"];
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
