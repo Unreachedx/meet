bfe739e1df0e311ec6205f7627b597ace3b6009d
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-09-24 18:13:10 +0200

    Updated App, App,css, setupTests and EventGenres to now work as intendet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import './App.css';
 
 const App = () => {
@@ -50,7 +51,10 @@ const App = () => {
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert} />
       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <div class="charts-container">
+      <EventGenresChart events={events}/>
       <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
  );
