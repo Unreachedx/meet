ab78fe4b6f8f9267d0fac013f66cebe52d2b1df5
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-09-24 17:26:36 +0200

    Updated CityEventCharts

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import {
   ScatterChart,
   Scatter,
@@ -11,37 +11,39 @@ import {
 const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
+  const getData = useCallback(() => {
+    return allLocations.map((location) => {
+      const count = events.filter((event) => event.location === location).length;
+      const city = location.split((/, | - /))[0];
+      return { city, count };
+    });
+  }, [allLocations, events]);
+
   useEffect(() => {
     setData(getData());
-  }, [`${data}`]);
-
-  const getData = () => {
-    const data = allLocations.map((location) => {
-      const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
-      return { city, count };
-    })
-    return data;
-  };
+  }, [getData]);
 
   return (
-    <ResponsiveContainer width="99%" height={400}>
-      <ScatterChart
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 20,
-          left: 20,
-        }}
-      >
+      <ResponsiveContainer width="99%" height={400}>
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 60,
+            left: -30,
+          }}
+        >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />`
+        <XAxis
+          type="category" dataKey="city" name="City"
+          angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
         <YAxis type="number" dataKey="count" name="Number of events" />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
     </ResponsiveContainer>
   );
-}
+};
 
 export default CityEventsChart;
